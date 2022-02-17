5dd9f2e77bf82ef63d93776f8dd0f4e456198bc0
Spencer Wolf <spencer.wolf@amperity.com>
2022-02-17 10:18:01 -0500

    Implement java switch function

--- zsh/init.sh
+++ zsh/init.sh
@@ -5,3 +5,11 @@ export EDITOR=vim
 
 #Prompt
 PROMPT='%F{51}%1d%f $'
+
+# Allow switching between java versions
+switch-java() {
+  export JAVA_HOME=$(/usr/libexec/java_home -v $1)
+  java -version
+}
+switch-java 1.8 2> /dev/null
+
