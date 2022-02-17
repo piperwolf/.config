0693996c97b3d697a6420dfbb894109b793cb350
Spencer Wolf <spencer.wolf@amperity.com>
2022-02-17 17:02:24 -0500

    Add autocompile to packer

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,11 +1,20 @@
 -- Only required if you have packer configured as `opt`
 -- vim.cmd [[packadd packer.nvim]]
+vim.cmd([[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerCompile
+  augroup end
+]])
 
 return require('packer').startup(function()
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
+  use {'dracula/vim', as = 'dracula'}
+
+  -- Clojure Plugins
   use 'google/vim-maktaba'
   use 'google/vim-glaive'
   use 'google/vim-codefmt'
-  use {'dracula/vim', as = 'dracula'}
+  use 'Olical/conjure'
 end)
