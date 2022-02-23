33c5c0c441bbc870b0b5f114f8239b10139c08a4
Spencer Wolf <spencer.wolf@amperity.com>
2022-02-23 17:35:32 -0500

    Move .lein into config folder

---
+++ .lein/profiles.clj
@@ -0,0 +1,7 @@
+{:user {:plugins [[lein-monolith "1.7.0"]
+                  [mvxcvi/whidbey "2.2.1"]
+                  [lein-cprint "1.3.3"]
+                  [lein-ancient "0.7.0"]
+                  [lein-cloverage "1.2.2"]
+                  [com.jakemccrary/lein-test-refresh "0.25.0"]
+                  [lein-monolith "1.7.0"]]}}
--- Makefile
+++ Makefile
@@ -1,4 +1,6 @@
 run: clean
 	-ln -s ~/.config/.zshrc ~/.zshrc
+	-ln -s ~/.config/.lein ~/.lein
 clean:
 	-unlink ~/.zshrc
+	-unlink ~/.lein
