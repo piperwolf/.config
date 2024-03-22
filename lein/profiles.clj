8ffe4bb5d1af945d1cbb9200b0068769fcac677b
Spencer Wolf <spencer.wolf@amperity.com>
2024-03-22 13:50:50 -0400

    Fix nukem2

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -5,7 +5,8 @@
                   [lein-ancient "0.7.0"]
                   [lein-cloverage "1.2.2"]
                   [com.jakemccrary/lein-test-refresh "0.25.0"]
-                  [amperity-service/lein-template "MONOLITH-SNAPSHOT"]]
+                  ;; This can break fresh installs
+                  #_[amperity-service/lein-template "MONOLITH-SNAPSHOT"]]
         :dependencies [[hashp "0.2.1"]
                        [pjstadig/humane-test-output "0.11.0"]
                        [vvvvalvalval/scope-capture "0.3.3"]]
--- setup.md
+++ setup.md
@@ -2,3 +2,7 @@
 
 - install tmux
 - install tmuxp `pip install --user tmuxp`
+
+# kitty
+
+git clone --depth 1 https://github.com/dexpota/kitty-themes.git ~/.config/kitty/kitty-themes
